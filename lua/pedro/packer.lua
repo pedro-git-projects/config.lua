7cd62d75d422c4b0aa62fa671fdc2c953ae8c7fd
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-07-25 16:41:49 -0300

    vimtex

--- lua/pedro/packer.lua
+++ lua/pedro/packer.lua
@@ -39,4 +39,5 @@ return require('packer').startup(function(use)
         }
     }
     use('windwp/nvim-autopairs')
+    use ('lervag/vimtex')
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -158,6 +158,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/home/pedro/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
+  },
+  vimtex = {
+    loaded = true,
+    path = "/home/pedro/.local/share/nvim/site/pack/packer/start/vimtex",
+    url = "https://github.com/lervag/vimtex"
   }
 }
 
