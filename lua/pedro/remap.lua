aea787168a0f2d6b52aed08e5f2151c7fcd52bd5
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-07-25 15:44:14 -0300

    undotree|fugitive

--- lua/pedro/remap.lua
+++ lua/pedro/remap.lua
@@ -1,2 +1,2 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- while in normal mode, pressing space pv will execute Ex
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex) -- while in normal mode, pressing space pv will execute Ex
