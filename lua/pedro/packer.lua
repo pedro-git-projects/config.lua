f4de25ebed07d25ff68aa77bdcbe66ea33e5141c
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-07-25 15:12:18 -0300

    colorscheme

--- lua/pedro/packer.lua
+++ lua/pedro/packer.lua
@@ -9,8 +9,9 @@ return require('packer').startup(function(use)
 
   use {
   'nvim-telescope/telescope.nvim', tag = '0.1.2',
--- or                            , branch = '0.1.x',
+   -- or                            , branch = '0.1.x',
   requires = { {'nvim-lua/plenary.nvim'} }
 }
 
+  use { "ellisonleao/gruvbox.nvim" }
 end)
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -74,6 +74,11 @@ end
 time([[try_loadstring definition]], false)
 time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
+  ["gruvbox.nvim"] = {
+    loaded = true,
+    path = "/home/pedro/.local/share/nvim/site/pack/packer/start/gruvbox.nvim",
+    url = "https://github.com/ellisonleao/gruvbox.nvim"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/home/pedro/.local/share/nvim/site/pack/packer/start/packer.nvim",
